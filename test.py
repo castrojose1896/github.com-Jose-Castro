d28caa435798af9e6fe8e2e5dc66b2f520fff674
castrojose1896 <castrojose1896@gmail.com>
2026-02-17 21:02:31 +1100

    Update test.py

--- test.py
+++ test.py
@@ -3,3 +3,16 @@ site_elevation=2600
 print(f"Project altitude is {site_elevation} meters")
 weatherForecast='HOT'
 type(weatherForecast)
+station_name = "South Melbourne"
+station_id = 132310
+temp=17.34545
+info_text=(f"the temperature at {station_name} (ID:{station_id})is {temp:.2f} Celcius.")
+text="Stations:South Melbourne, North Melbourne, East Melbourne"
+splitted=text.split(":")
+splitted
+['Stations','South Melbourne, North Melbourne, East Melbourne']
+type(splitted)
+list
+stations_text=splitted[1]
+stations_text
+print(info_text)
